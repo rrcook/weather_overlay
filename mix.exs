eb9bc5a72e0899b8674b98325c33105a17080e1f
Richard Cook <blubparadox@gmail.com>
2024-04-01 13:53:30 -0400

    Commit before using EnumTypes.

--- mix.exs
+++ mix.exs
@@ -21,6 +21,9 @@ defmodule NaplpsWriter.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:httpoison, "~> 1.8.2"},
+      {:elixir_xml_to_map, "~> 3.1"},
+      {:enum_type, "~> 1.1.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
